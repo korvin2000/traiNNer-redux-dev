Fix compile and mixed-precision training bugs

traiNNer/models/sr_model.py
@@ -469,7 +469,7 @@ class SRModel(BaseModel):
         loss_dict: dict[str, Tensor | float] = OrderedDict()
 
         lq = rgb2pixelformat_pt(self.lq, self.opt.input_pixel_format)
-        rgb2pixelformat_pt(self.gt, self.opt.input_pixel_format)
+        self.gt = rgb2pixelformat_pt(self.gt, self.opt.input_pixel_format)
 
         with torch.autocast(
             device_type=self.device.type, dtype=self.amp_dtype, enabled=self.use_amp
@@ -655,7 +655,7 @@ class SRModel(BaseModel):
                         for sublabel, loss_val in l_g_loss.items():
                             if loss_val > 0:
                                 weighted_loss_val = loss_val * adjusted_weight
-                                l_g_total += weighted_loss_val * self.accum_iters
+                                l_g_total += weighted_loss_val / self.accum_iters
                                 loss_dict[f"{label}_{sublabel}"] = weighted_loss_val
                     else:
                         weighted_l_g_loss = l_g_loss * adjusted_weight